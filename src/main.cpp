656c0efb23fffc38a1f1a8ba76d91f1953951bc7
garrettkinman <garrettkinman@gmail.com>
2021-11-21 01:41:10 -0500

    added small test statement to test out the library #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <Arduino.h>
+#include <LoRaMessage.h>
 
 void setup() {
   // put your setup code here, to run once:
+
+
+  // test if the private LoRaMessage library works
+  RegularMessage message(1, 0, 0, 10000000, "hello, world");
 }
 
 void loop() {
