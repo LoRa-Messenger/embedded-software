d6e2f1afbddf4fe23408e88acb261c5d500c8df8
garrettkinman <garrettkinman@gmail.com>
2021-11-26 16:01:02 -0500

    updated the baudrate to heltec default #1

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,19 @@
 #include <SPI.h>
 #include <Arduino.h>
 #include <LoRaMessage.h>
+#include "heltec.h"
 
-#define BAUDRATE 9600
+#define BAND 915E6 // LoRa frequency band
 
 void setup() {
   // put your setup code here, to run once:
 
-  Serial.begin(BAUDRATE);
+  // enable OLED display, LoRa, UART serial, and PABOOST; set LoRa frequency band
+  // note that this sets serial baudrate to 115200
+  Heltec.begin(true, true, true, true, BAND);
 
   // test if the private LoRaMessage library works
   RegularMessage message(1, 0, 0, 10000000, "hello, world");
-  Serial.println("hello, world");
 }
 
 void loop() {
