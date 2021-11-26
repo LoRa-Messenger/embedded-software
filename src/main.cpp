7edb4d10c008a3cd8759a14804ed4c35c70d8824
garrettkinman <garrettkinman@gmail.com>
2021-11-26 15:41:49 -0500

    added serial print, fixed libraries #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
+#include <Wire.h>
+#include <SPI.h>
 #include <Arduino.h>
 #include <LoRaMessage.h>
+
+#define BAUDRATE 9600
 
 void setup() {
   // put your setup code here, to run once:
 
+  Serial.begin(BAUDRATE);
 
   // test if the private LoRaMessage library works
   RegularMessage message(1, 0, 0, 10000000, "hello, world");
+  Serial.println("hello, world");
 }
 
 void loop() {
