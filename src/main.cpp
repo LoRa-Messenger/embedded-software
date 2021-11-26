0cb427818d8ad1e9dff08f9cd05e8f83f8e078d1
garrettkinman <garrettkinman@gmail.com>
2021-11-26 18:09:57 -0500

    LoRa transmission works #1

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 
 #define BAND 915E6 // LoRa frequency band
 
+int counter = 0;
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -19,4 +21,26 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+  // send packet
+  LoRa.beginPacket();
+/*
+* LoRa.setTxPower(txPower,RFOUT_pin);
+* txPower -- 0 ~ 20
+* RFOUT_pin could be RF_PACONFIG_PASELECT_PABOOST or RF_PACONFIG_PASELECT_RFO
+*   - RF_PACONFIG_PASELECT_PABOOST -- LoRa single output via PABOOST, maximum output 20dBm
+*   - RF_PACONFIG_PASELECT_RFO     -- LoRa single output via RFO_HF / RFO_LF, maximum output 14dBm
+*/
+  LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST);
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+  
+  counter++;
+  digitalWrite(25, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(25, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000); 
 }
